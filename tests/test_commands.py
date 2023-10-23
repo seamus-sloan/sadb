f55a9e6b7042d4621e12da52c7acb1d277de9384
Seamus <seamus.sloan@gmail.com>
2023-10-23 18:42:19 -0400

    fix record tests & add delete recording test

--- tests/test_commands.py
+++ tests/test_commands.py
@@ -16,6 +16,8 @@ from sadb import scrcpy, get_ip, screenshot, record, wifi, search
 TEST_APK = "myApp.apk"
 TEST_PACKAGE = "com.example.app"
 TEST_DEVICE = DEVICE_IDS[0]
+DEFAULT_VIDEO_NAME = "video.mp4"
+DEFAULT_VIDEO_SAVE_LOC = "/data/local/tmp/"
 
 
 def test_stop():
@@ -90,38 +92,95 @@ def test_screenshot_custom_name():
             stdout=mock_file.return_value)
 
 
-def test_record_custom_name():
-    filename = "custom.mp4"
-    remote_path = "/data/local/tmp/screenrecord.mp4"
-
-    mock_proc = MagicMock()
-    with patch("subprocess.Popen", return_value=mock_proc) as mock_popen, \
-            patch("subprocess.run") as mock_run, \
-            patch("time.sleep", side_effect=[None, None, KeyboardInterrupt]) as mock_sleep:
-        record(TEST_DEVICE, filename)
-        mock_popen.assert_called_once_with(
-            ["adb", "-s", TEST_DEVICE, "shell", f"screenrecord {remote_path}"])
-        assert call(["adb", "-s", TEST_DEVICE, "pull", remote_path,
-                    filename]) in mock_run.call_args_list
-        assert call(["adb", "-s", TEST_DEVICE, "shell",
-                    f"rm {remote_path}"]) in mock_run.call_args_list
-
-
 def test_record_default_name():
-    filename = "video.mp4"
-    remote_path = "/data/local/tmp/screenrecord.mp4"
+    expected_file_location = f"{DEFAULT_VIDEO_SAVE_LOC}{DEFAULT_VIDEO_NAME}"
 
-    mock_proc = MagicMock()
-    with patch("subprocess.Popen", return_value=mock_proc) as mock_popen, \
-            patch("subprocess.run") as mock_run, \
-            patch("time.sleep", side_effect=[None, None, KeyboardInterrupt]) as mock_sleep:
+    with patch("sadb.subprocess") as mock_subprocess, \
+         patch("time.sleep", side_effect=[None, KeyboardInterrupt, None]) as mock_sleep, \
+         patch("sadb.input", return_value='y') as mock_input:
+
+        mock_popen = MagicMock()
+        mock_subprocess.Popen.return_value = mock_popen
+        mock_subprocess.run.return_value = MagicMock(returncode=0)
+
+        # Call the function
         record(TEST_DEVICE, "")
-        mock_popen.assert_called_once_with(
-            ["adb", "-s", TEST_DEVICE, "shell", f"screenrecord {remote_path}"])
-        assert call(["adb", "-s", TEST_DEVICE, "pull", remote_path,
-                    filename]) in mock_run.call_args_list
-        assert call(["adb", "-s", TEST_DEVICE, "shell",
-                    f"rm {remote_path}"]) in mock_run.call_args_list
+
+        # Check if subprocess.Popen was called with correct arguments
+        mock_subprocess.Popen.assert_called_once_with(["adb", "-s", TEST_DEVICE, "shell", \
+                                                       f"screenrecord {expected_file_location}"])
+
+        # Check if KeyboardInterrupt was handled correctly
+        mock_popen.terminate.assert_called_once()
+
+        # Check if subprocess.run was called with correct arguments for pulling the file
+        assert mock_subprocess.run.call_args_list[0] == \
+            call(["adb", "-s", TEST_DEVICE, "pull", expected_file_location, DEFAULT_VIDEO_NAME])
+
+        # Check if subprocess.run was called with correct arguments for deleting the file
+        assert mock_subprocess.run.call_args_list[1] == \
+            call(["adb", "-s", TEST_DEVICE, "shell", f"rm {expected_file_location}"])
+
+
+def test_record_custom_name():
+    custom_name = "customName.mp4"
+    expected_file_location = f"{DEFAULT_VIDEO_SAVE_LOC}{custom_name}"
+
+    with patch("sadb.subprocess") as mock_subprocess, \
+         patch("time.sleep", side_effect=[None, KeyboardInterrupt, None]) as mock_sleep, \
+         patch("sadb.input", return_value='y') as mock_input:
+
+        mock_popen = MagicMock()
+        mock_subprocess.Popen.return_value = mock_popen
+        mock_subprocess.run.return_value = MagicMock(returncode=0)
+
+        # Call the function
+        record(TEST_DEVICE, custom_name)
+
+        # Check if subprocess.Popen was called with correct arguments
+        mock_subprocess.Popen.assert_called_once_with(["adb", "-s", TEST_DEVICE, "shell", \
+                                                       f"screenrecord {expected_file_location}"])
+
+        # Check if KeyboardInterrupt was handled correctly
+        mock_popen.terminate.assert_called_once()
+
+        # Check if subprocess.run was called with correct arguments for pulling the file
+        assert mock_subprocess.run.call_args_list[0] == \
+            call(["adb", "-s", TEST_DEVICE, "pull", expected_file_location, custom_name])
+
+        # Check if subprocess.run was called with correct arguments for deleting the file
+        assert mock_subprocess.run.call_args_list[1] == \
+            call(["adb", "-s", TEST_DEVICE, "shell", f"rm {expected_file_location}"])
+
+
+def test_record_without_deleting_file():
+    expected_file_location = f"{DEFAULT_VIDEO_SAVE_LOC}{DEFAULT_VIDEO_NAME}"
+
+    with patch("sadb.subprocess") as mock_subprocess, \
+         patch("time.sleep", side_effect=[None, KeyboardInterrupt, None]) as mock_sleep, \
+         patch("sadb.input", return_value='n') as mock_input:
+
+        mock_popen = MagicMock()
+        mock_subprocess.Popen.return_value = mock_popen
+        mock_subprocess.run.return_value = MagicMock(returncode=0)
+
+        # Call the function
+        record(TEST_DEVICE, DEFAULT_VIDEO_NAME)
+
+        # Check if subprocess.Popen was called with correct arguments
+        mock_subprocess.Popen.assert_called_once_with(["adb", "-s", TEST_DEVICE, "shell", \
+                                                       f"screenrecord {expected_file_location}"])
+
+        # Check if KeyboardInterrupt was handled correctly
+        mock_popen.terminate.assert_called_once()
+
+        # Check if subprocess.run was called with correct arguments for pulling the file
+        assert mock_subprocess.run.call_args_list[0] == \
+            call(["adb", "-s", TEST_DEVICE, "pull", "/data/local/tmp/video.mp4", "video.mp4"])
+
+        # Check if subprocess.run was not called with arguments for deleting the file
+        delete_call = call(["adb", "-s", TEST_DEVICE, "shell", f"rm {expected_file_location}"])
+        assert delete_call not in mock_subprocess.run.call_args_list
 
 
 def test_wifi():
